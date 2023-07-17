5f0193f3e998739aac63c31888b1c03ef79db8d4
student <student@gnu.ac.in>
2023-07-17 08:15:59 +0530

    pr1_3_21012011094.kt

---
+++ pr1_4_21012011094.kt
@@ -0,0 +1,13 @@
+fun main()
+{
+    println("Enter a number: ")
+    var a = readLine()!!.toInt()
+    if (a%2==0)
+    {
+        println("$a is even")
+    }
+    else
+    {
+        println("$a is odd")
+    }
+}
