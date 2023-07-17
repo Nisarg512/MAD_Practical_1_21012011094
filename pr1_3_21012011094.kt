3648c756c3670c31eac9cd700be5e280100f4d66
student <student@gnu.ac.in>
2023-07-17 08:07:39 +0530

    pr1_3

---
+++ pr1_3_21012011094.kt
@@ -0,0 +1,20 @@
+fun main()
+{
+    println("Student Enrolment: ")
+    var a= readLine()
+    println("Student Name: ")
+    var b=readLine()
+    println("Student branch: ")
+    var c=readLine()
+    println("Student class: ")
+    var d=readLine()
+    println("Student batch: ")
+    var e=readLine()
+    println("Student Collage name: ")
+    var f=readLine()
+    println("Student university name: ")
+    var g=readLine()
+    println("Student age: ")
+    var h=readLine()
+
+}
